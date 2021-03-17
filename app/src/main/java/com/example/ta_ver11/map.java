cb8e87b73034cc645451e345e38bf0a9df3a6f56
Dendi Santoso <67273454+dendisantoso74@users.noreply.github.com>
2021-03-18 01:49:02 +0700

    user location sudah masuk, navigasi jalan tapi destination masih dipilih pada map

--- app/src/main/java/com/example/ta_ver11/map.java
+++ app/src/main/java/com/example/ta_ver11/map.java
@@ -3,19 +3,62 @@ package com.example.ta_ver11;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncher;
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions;
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
 
-public class map extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import timber.log.Timber;
+
+public class map extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener {
+
+    private static final String DESTINATION_SYMBOL_LAYER_ID = "destination-symbol-layer-id";
+    private static final String DESTINATION_ICON_ID = "destination-icon-id";
+    private static final String DESTINATION_SOURCE_ID = "destination-source-id";
+    private static final int REQUEST_CODE_AUTOCOMPLETE = 1;
+
+    private PermissionsManager permissionsManager;
     private MapView mapView;
+    private MapboxMap mapboxMap;
+    private Point origin, destination;
+    private Button BtnStart;
+    private NavigationMapRoute navigationMapRoute;
+    private DirectionsRoute currentRoute;
+    private Marker destinationMarker;
+    private static final String TAG = "opsiIsoman";
+    private int status = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,25 +69,114 @@ public class map extends AppCompatActivity {
         setContentView(R.layout.activity_map);
 
         mapView = (MapView) findViewById(R.id.mapView);
+
         mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
+        mapView.getMapAsync(this);
+        BtnStart = findViewById(R.id.btnStart);
 
+        BtnStart.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+            public void onClick(View view) {
+                if(status != 1) {
+                    NavigationLauncherOptions options = NavigationLauncherOptions.builder()
+                            .directionsRoute(currentRoute)
+                            .shouldSimulateRoute(true)
+                            .build();
+                    NavigationLauncher.startNavigation(map.this, options);
+                }else if(status == 1){
+                    status = 0;
+                    getNavigation(origin,destination);
+                }
+            }
+        });
 
-                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded(){
+
+
+    }
+
+    //permission
+    @SuppressLint("MissingPermission")
+    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            LocationComponent locationComponent = mapboxMap.getLocationComponent();
+
+            locationComponent.activateLocationComponent(
+                    LocationComponentActivationOptions.builder(this, loadedMapStyle).build()
+            );
+
+            locationComponent.setLocationComponentEnabled(true);
+
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+            this.origin = Point.fromLngLat(locationComponent.getLastKnownLocation().getLongitude(),
+                    locationComponent.getLastKnownLocation().getLatitude());
+        }else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    //new override
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(this, "Grant Location Permission", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            mapboxMap.getStyle(new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) { enableLocationComponent(style); }
+            });
+        }else {
+            Toast.makeText(this, "Permission not granted", Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull MapboxMap mapboxMap) {
+        this.mapboxMap = mapboxMap;
+
+        mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded(){
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                List<Feature> symbolLayerIconFeatureList = new ArrayList<>();
+                enableLocationComponent(style);
+
+                //initLayers(style);
+
+                mapboxMap.addOnMapClickListener(new MapboxMap.OnMapClickListener() {
                     @Override
-                    public void onStyleLoaded(@NonNull Style Style){
-                        Toast.makeText(map.this, "map loaded", Toast.LENGTH_SHORT).show();
+                    public boolean onMapClick(@NonNull LatLng point) {
+                        if(destinationMarker != null) {
+                            mapboxMap.removeMarker(destinationMarker);
+                        }
+                        destinationMarker = mapboxMap.addMarker(new MarkerOptions().position(point));
+                        destination = Point.fromLngLat(point.getLongitude(),point.getLatitude());
+                        origin = Point.fromLngLat(origin.longitude(),origin.latitude());
+                        BtnStart.setEnabled(true);
+                        BtnStart.setBackgroundResource(R.color.mapbox_blue);
 
+                        getRoute(origin,destination);
+                        return true;
                     }
-                        });
-
+                });
             }
         });
 
     }
 
+
+
+    //on start
     @Override
     protected void onStart() {
         super.onStart();
@@ -86,4 +218,80 @@ public class map extends AppCompatActivity {
         super.onDestroy();
         mapView.onDestroy();
     }
+
+    //navigation
+    private void getNavigation(Point originL, Point destination) {
+        NavigationRoute.builder(getApplicationContext())
+                .accessToken(getString(R.string.mapbox_access_token))
+                .origin(originL)
+                .destination(destination)
+                .build()
+                .getRoute(new Callback<DirectionsResponse>() {
+                    @Override
+                    public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                        Timber.d("Response code: " + response.code());
+                        if (response.body() == null) {
+                            Toast.makeText(map.this, "No routes found, make sure you set the right user and access token.", Toast.LENGTH_SHORT).show();
+                            return;
+                        } else if (response.body().routes().size() < 1) {
+                            Toast.makeText(map.this, "No routes found", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        currentRoute = response.body().routes().get(0);
+
+                        NavigationLauncherOptions options = NavigationLauncherOptions.builder()
+                                .directionsRoute(currentRoute)
+                                .shouldSimulateRoute(true)
+                                .build();
+                        NavigationLauncher.startNavigation(map.this, options);
+                    }
+
+                    @Override
+                    public void onFailure(Call<DirectionsResponse> call, Throwable t) {
+                        Toast.makeText(map.this, "Error: " + t.getMessage(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    //routing
+    private void getRoute(Point origin, Point destination) {
+        NavigationRoute.builder(this)
+                .accessToken(Mapbox.getAccessToken())
+                .origin(origin)
+                .destination(destination)
+                .build()
+                .getRoute(new Callback<DirectionsResponse>() {
+                    @Override
+                    public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                        // You can get the generic HTTP info about the response
+                        Timber.d("Response code: " + response.code());
+                        if (response.body() == null) {
+                            Timber.e("No routes found, make sure you set the right user and access token.");
+                            return;
+                        } else if (response.body().routes().size() < 1) {
+                            Timber.e("No routes found");
+                            return;
+                        }
+
+                        // Get the directions route
+                        currentRoute = response.body().routes().get(0);
+                        if(navigationMapRoute != null){
+                            navigationMapRoute.removeRoute();
+                        }else {
+                            navigationMapRoute = new NavigationMapRoute(null, mapView,mapboxMap );
+                        }
+                        navigationMapRoute.addRoute(currentRoute);
+                    }
+
+                    @Override
+                    public void onFailure(Call<DirectionsResponse> call, Throwable throwable) {
+                        Timber.e("Error: " + throwable.getMessage());
+                        Toast.makeText(map.this, "Error: " + throwable.getMessage(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
 }
